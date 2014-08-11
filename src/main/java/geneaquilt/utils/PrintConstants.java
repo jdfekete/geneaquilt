ce1f82f53c3ecf37beb4a7afe43d1931716423ce
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2014-08-11 16:20:33 +0200

    added license header

--- src/main/java/geneaquilt/utils/PrintConstants.java
+++ src/main/java/geneaquilt/utils/PrintConstants.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2010-2014, Jean-Daniel Fekete, Pierre Dragicevic, and INRIA.
+ * All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 package geneaquilt.utils;
 
 import edu.umd.cs.piccolo.util.PPaintContext;
